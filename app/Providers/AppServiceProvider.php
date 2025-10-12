4809c777b5e7c8e62b4863fbad21d31725af7d94
Anas Nur Fauzan <anasnurf99@gmail.com>
2025-10-12 15:45:48 +0700

    Change vercel.json

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,9 +1,9 @@
 <?php
 
 namespace App\Providers;
-
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,5 +21,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Schema::defaultStringLength(191);
+        if ($this->app->environment('production')) {
+        URL::forceScheme('https');
+    }
     }
 }
