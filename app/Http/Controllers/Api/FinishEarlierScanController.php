e6eff233e3029141e8c97c9dcd8beba065898f63
anasnurf99 <anasnurf99@gmail.com>
2026-09-18 00:19:24 +0700

    Update OCR model for finish earlier scanner

--- app/Http/Controllers/Api/FinishEarlierScanController.php
+++ app/Http/Controllers/Api/FinishEarlierScanController.php
@@ -29,7 +29,7 @@ class FinishEarlierScanController extends Controller
                 'Content-Type'  => 'application/json',
             ],
             'json' => [
-                'model'    => 'mistral-ocr-latest',
+                'model'    => 'mistral-ocr-4-0',
                 'document' => ['type' => $type, $type => $dataUrl],
             ],
         ]);
